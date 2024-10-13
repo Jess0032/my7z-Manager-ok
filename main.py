92330f4bfb8fc310f0527c20ef55737e749c557a
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2024-10-13 06:51:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -246,17 +246,17 @@ async def download_from_url(client, message):
         return
 
     # Ask for the filename
-    prompt_message = await message.reply_text(
-        "Enter the filename to save as (include the extension):"
-    )
-    response = await client.listen(user_id, filters=filters.text)
-    await prompt_message.delete()
-    await response.delete()
-    filename = response.text.strip()
+    #prompt_message = await message.reply_text(
+     #   "Enter the filename to save as (include the extension):"
+    #)
+    #response = await client.listen(user_id, filters=filters.text)
+    #await prompt_message.delete()
+    #await response.delete()
+    #filename = response.text.strip()
 
-    dirpath = pathlib.Path(f"{user_id}/files")
-    dirpath.mkdir(parents=True, exist_ok=True)
-    filepath = dirpath / filename
+    #dirpath = pathlib.Path(f"{user_id}/files")
+    #dirpath.mkdir(parents=True, exist_ok=True)
+    #filepath = dirpath / filename
 
     progress_message = await message.reply_text("Starting download...")
 
