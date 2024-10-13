8af5c115eeb7bd170f86f56c73acf272ce236967
qwerty59b <130035937+qwerty59b@users.noreply.github.com>
2024-10-13 07:55:15 -0400

    lol rath

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from humanize import naturalsize
 from pyrogram import Client, filters, idle
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pyromod import listen
+from functions import *
 
 # Replace with your actual API credentials
 API_ID: int = int(os.environ.get("API_ID"))
